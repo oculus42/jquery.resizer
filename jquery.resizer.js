a53c080e662c4588058a50bfbbc8e092d5c6a096
oculus42 <oculus.habent@gmail.com>
2013-08-29 18:13:47 -0400

    v2 - jQuery 1.5+ rework & simplify.
    Just playing with it, really. Removed the options/settings, since there's only one (interval).

--- jquery.resizer.js
+++ jquery.resizer.js
@@ -1,64 +1,49 @@
 /* *******************************************************************
 jquery.resizer: Simple jQuery Window Resize Handler
-Throttles the window.resize event and pushes it to other elements.
-(c) 2012 Samuel Rouse
+Delays/throttles the window.resize event and pushes it to other elements.
+
+2012-2013 Samuel Rouse
+
 http://github.com/oculus42/jquery.resizer    
 ******************************************************************* */
 
-(function ($) {
+(function (window,$) {
+    "use strict";
 
-    var methods = {
-        
-        resizer : function() {
-        	$(methods.items).trigger('resizer');
-        },
-        
-        timer : 0,
-        
-        items : []
-        
-    };
+    var resizer = function() {
+        $(items).trigger('resizer');
+    }, timer = 0, items = [];
 
     $.fn.resizer = function (param1, param2) {
-        
-		var options = {};
-		var bindfunc = undefined;
-		
-		switch (typeof param1) {
-			case "object":
-				options = param1;
-				break;
-			case "number":
-				options = { interval : param1 }; 
-				break;
-			case "function":
-				bindfunc = param1;
-				break;
-			default:
-				console && console.log("jqResizer: param1 type " + (typeof param1));
-		}
-		
-		if (typeof param2 === "function" ) { bindfunc = param2; }
-		
-		// Set the settings.
-		var settings = $.extend({
-			interval: 100
-		}, options);
-		
-		$(window).bind('resize',function(){
-			methods.timer === 0 || clearTimeout(methods.timer);
-			methods.timer = setTimeout(function(){methods.resizer()}, settings.interval);
-		    
-		});
-		
-		// Add it to the items array and bind the function if one was passed.
-		return this.each(function (idx, el) {
-			methods.items.push(el);
-			
-			if (bindfunc !== undefined) {
-		    	$(this).bind('resizer',bindfunc);
-			}
-		});
-	};
 
-})(jQuery);
+	var interval = 100, bindfunc;
+		
+	switch (typeof param1) {
+		case "number":
+			interval = +param1; 
+			break;
+		case "function":
+			bindfunc = param1;
+			break;
+		default:
+			console && console.log("jqResizer: arugment type " + (typeof param1));
+	}
+		
+	if (typeof param2 === "function" ) { bindfunc = param2; }
+	
+	$(window).on('resize',function(){
+		timer === 0 || clearTimeout(timer);
+		timer = setTimeout(function(){resizer()}, interval);
+	});
+	
+	// Add it to the items array and bind the function if one was passed.
+	return this.each(function (idx, el) {
+		items.push(el);
+		
+		if (bindfunc !== undefined) {
+	    		$(el).on('resizer',bindfunc);
+		}
+	});
+    };
+
+})(window,jQuery);
